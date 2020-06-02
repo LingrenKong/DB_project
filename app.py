779f61dc729058e06fa3dabb3e288d05b36a6a93
LingrenKong <1551086871@qq.com>
2020-06-02 17:03:03 +0800

    加入了用户界面（初步），包含用户登录、无登录查书、借阅（暂时没有借阅的响应程序）； 完善了管理员界面并且改善了boostrap页面设计

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, session
 from flask import Response, request, render_template_string,  redirect, Blueprint,render_template,send_file
 #画图功能
 from matplotlib.backends.backend_agg import FigureCanvasAgg
@@ -6,7 +6,7 @@ from matplotlib.backends.backend_svg import FigureCanvasSVG
 from matplotlib.figure import Figure
 
 app = Flask(__name__)
-
+app.config["SECRET_KEY"] = "klr"#设置一个key才可以有session，否则session是none
 #from flask.ext.bootstrap import Bootstrap
 #Bootstrap(app)  # 把程序实例即 app 传入构造方法进行初始化
 
@@ -32,7 +32,7 @@ def get_cursor():
 def hello_world():
     return render_template('index.html')
 
-@app.route('/boostrap_demo/')
+@app.route('/boostrap-demo/')
 def try_boostrap():
     return render_template('base.html')
 
@@ -40,13 +40,52 @@ def try_boostrap():
 def admin():
     return render_template('admin.html')
 
-@app.route('/admin/summary/')
+@app.route('/admin/book-summary/')
 def book_summary():
     cursor = get_cursor()
     exist = cursor.execute("SELECT * FROM book WHERE removed=0").fetchall()
     cursor = get_cursor()
     removed = cursor.execute("SELECT * FROM book WHERE removed=1").fetchall()
     return render_template('book_summary.html',exist = exist,removed=removed)
+
+@app.route('/user-control/',methods = ['POST', 'GET'])
+def user():
+    mode = None
+    cursor = get_cursor()
+    if request.method == 'GET':
+        mode = 1
+    if request.method == 'POST':
+        id = request.form['id']
+        password = request.form['password']
+        result = cursor.execute(f"SELECT * FROM Userlist WHERE Urestrict=0 AND id='{id}' AND Upassword='{password}' ")
+        if result.fetchone():
+            mode = 2 #登录有效
+            session['user_id'] = id#后面都可以使用了
+        else:
+            mode = 3
+            session['user_id'] = None
+    return render_template('user.html',mode=mode)
+
+@app.route('/user-control/search/',methods = ['POST', 'GET'])
+def book_search():
+    # 这个搜索不需要登录要求
+    search = None
+    cursor = get_cursor()
+    if request.method == 'POST':
+        form = request.form
+        search = cursor.execute(f"SELECT * FROM book WHERE removed=0 AND Bname LIKE '%{form.get('Bname')}%'")
+    return render_template('book_search.html',search=search)
+
+@app.route('/user-control/borrow/',methods = ['POST', 'GET'])
+def book_borrow():
+    # 借阅需要登录
+    ISBN = None
+    cursor = get_cursor()
+    print(session.get('user_id'))
+    if not session.get('user_id') or session['user_id']==None:
+        #print(session.get('user_id'))
+        return redirect('/user-control/')
+    return render_template('book_borrow.html',id=session.get('user_id'))
 
 if __name__ == '__main__':
     app.run(debug=True)
